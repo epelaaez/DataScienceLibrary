9d7ba9c46cebce4d743cb8cebcd78a3eacfe7756
Emilio <epelaaez@gmail.com>
2020-11-15 17:47:22 +0100

    Structure for test files

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import data_scilib.functions as f
 
 def tests():
-    print(f.find_value_in_row('tests.csv', 'Username', 'Sam'))
-    print(f.is_empty('tests.csv', 'Last name'))
+    print(f.find_value_in_row('tests_files/small_dataset.csv', 'Username', 'Sam'))
+    print(f.is_empty('tests_files/small_dataset.csv', 'Last name'))
 
 if __name__ == '__main__':
     tests()
