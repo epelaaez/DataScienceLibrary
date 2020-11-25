57fe318cbcc0b5280269db9361ecaa8735c0bc6b
Emilio <epelaaez@gmail.com>
2020-11-25 14:19:08 +0100

    Add class to functions

--- data_scilib/functions.py
+++ data_scilib/functions.py
@@ -1,123 +1,122 @@
 import csv
 
-def print_all(file):
-  """
-  This function prints the whole csv file
-  """
-  try:
-    with open(file, newline = '') as csvfile:
-      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-      for row in spamreader:
-        print(', '.join(row))
-  except FileNotFoundError:
-    print(f'File "{file}" not found in function print_all')
+class analyzer(object):
+  def __init__(self, path):
+    try:
+      self.file = open(path, newline = '')
+    except FileNotFoundError:
+      raise Exception(f'File "{path}" not found when initiating class')
 
-def print_no_header(file):
-  """
-  This function prints the whole csv file except for the first row (header)
-  """
-  try:
-    with open(file, newline='') as csvfile:
-      spamreader=csv.reader(csvfile,delimiter=',', quotechar='|')
-      next(spamreader) # Skips first row
-      for row in spamreader:
-        print(', '.join(row))
-  except FileNotFoundError:
-    print(f'File "{file}" not found in function print_no_header')
+  def close(self):
+    """
+    Closes file, after this function is executed you cannot longer read the file.
+    """
+    self.file.close()
 
-def rows_num(file):
-  """
-  This function returns the number of rows in the csv file
-  """
-  with open(file, newline='') as csvfile:
-    spamreader=csv.reader(csvfile,delimiter=',', quotechar='|')
+  def print_all(self):
+    """
+    This function prints the whole csv file
+    """
+    spamreader = csv.reader(self.file, delimiter = ',', quotechar = '|')
+    for row in spamreader:
+      print(', '.join(row))
+
+  def print_no_header(self):
+    """
+    This function prints the whole csv file except for the first row (header)
+    """
+    spamreader = csv.reader(self.file, delimiter=',', quotechar='|')
+    next(spamreader) # Skips first row
+    for row in spamreader:
+      print(', '.join(row))
+
+  def rows_num(self):
+    """
+    This function returns the number of rows in the csv file
+    """
+    spamreader = csv.reader(self.file, delimiter=',', quotechar='|')
     return(len(list(spamreader)) - 1) # subtract 1 to not count header
-    
+      
     # TODO: check for empty rows
     
-def columns_num(file):
-  """
-  This function returns the number of columns in the csv file
-  """
-  with open(file, newline='') as csvfile:
-    spamreader=csv.reader(csvfile,delimiter=',', quotechar='|')
+  def columns_num(self):
+    """
+    This function returns the number of columns in the csv file
+    """
+    spamreader = csv.reader(self.file, delimiter=',', quotechar='|')
     return(len((list(spamreader))[0]))
 
-def find_element(file, element):
-  """
-  This function will return a list containing the rows where the specified element appears
-  """
-  rows = []
+  def find_element(self, element):
+    """
+    This function will return a list containing the rows where the specified element appears
+    """
+    rows = []
 
-  with open(file, newline='')as csvfile:
-    spamreader=csv.reader(csvfile,delimiter=',', quotechar='|')
+    spamreader = csv.reader(self.file, delimiter=',', quotechar='|')
     next(spamreader) # Skips first row
     for row in spamreader:
       if element in row: # Check each row to check if element is present
         rows.append(row)          
-          
-  return rows
+            
+    return rows
 
-def find_value_in_row(file, column, element):
-  """
-  This function will find the value of the column specified by "column" in the row where the "element" is present. The "column" parameter needs to be in the first row of the csv file.
-  """
-  column_index = -1
-  found = []
+  def find_value_in_row(self, column, element):
+    """
+    This function will find the value of the column specified by "column" in the row where the "element" is present. The "column" parameter needs to be in the first row of the csv file.
+    """
+    column_index = -1
+    found = []
 
-  with open(file, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+    spamreader = csv.reader(self.file, delimiter = ',', quotechar = '|')
     header = next(spamreader)
     for i in range(len(header)):
       if header[i] == column:
         column_index = i
         break
 
-  # If the "column" was not found on the first row of the file, raise an exception
-  if column_index == -1:
-    raise Exception(f"Column '{column}' not found")
+    # If the "column" was not found on the first row of the file, raise an exception
+    if column_index == -1:
+      raise Exception(f"Column '{column}' not found")
 
-  rows = find_element(file, element) # Use already defined function to find the rows where "element" appears
+    rows = self.find_element(element) # Use already defined function to find the rows where "element" appears
 
-  for row in rows:
-    found.append(row[column_index])
+    for row in rows:
+      found.append(row[column_index])
 
-  return found
+    return found
 
-def is_empty(file, column):
-  """
-  This function will check if any row is empty on the "column" specified. If it is empty, it will add the row index (0 based, index 0 is header row) to the array returned.
-  """
-  column_index = -1
-  empty = []
+  def is_empty(self, column):
+    """
+    This function will check if any row is empty on the "column" specified. If it is empty, it will add the row index (0 based, index 0 is header row) to the array returned.
+    """
+    column_index = -1
+    empty = []
 
-  with open(file, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+    spamreader = csv.reader(self.file, delimiter = ',', quotechar = '|')
     header = next(spamreader)
     for i in range(len(header)):
       if header[i] == column:
         column_index = i
         break
-    
+      
     counter = 0
     for row in spamreader:
       counter += 1
       if row[column_index].strip() == '':
         empty.append(counter)
 
-  return empty 
+    return empty 
 
 
-def count_if(file, conditions):
-  """
-  Checks how many rows (excluding the header) satisfy the condition given by a lambda function in the column specified.
-  The input type is of array to enable multiple conditions in multiple columns. The array should be of tuples in the form: (column, condition), where column is a string and condition a lambda function
-  """
-  column_indeces = [-1] * len(conditions)
-  result = [0] * len(conditions)
+  def count_if(self, conditions):
+    """
+    Checks how many rows (excluding the header) satisfy the condition given by a lambda function in the column specified.
+    The input type is of array to enable multiple conditions in multiple columns. The array should be of tuples in the form: (column, condition), where column is a string and condition a lambda function
+    """
+    column_indeces = [-1] * len(conditions)
+    result = [0] * len(conditions)
 
-  with open(file, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+    spamreader = csv.reader(self.file, delimiter = ',', quotechar = '|')
     header = next(spamreader)
 
     for index, condition in enumerate(conditions):
@@ -137,22 +136,21 @@ def count_if(file, conditions):
                 result[index] += 1
             except Exception:
               pass
-  
-  return result
+    
+    return result
 
-def max_min(file, columns):
-  """
-  This function will return the maximum value and minimum value of each column specified in the "column" paramater. This parameter should be an array of strings with all the columns desired.
-  The return value is an array of arrays, each inside array with contain the maximum and minimum value of the column in the same index in the "columns" paramter.
-  IMPORTANT: the columns specified must have numerical values in at least 2 rows. It will return ['-inf', 'inf'] for columns that have no numerical values.
-  """
-  column_indeces = [-1] * len(columns)
-  result = []
-  for _ in range(len(columns)):
-    result.append([float('-inf'), float('inf')])
+  def max_min(self, columns):
+    """
+    This function will return the maximum value and minimum value of each column specified in the "column" paramater. This parameter should be an array of strings with all the columns desired.
+    The return value is an array of arrays, each inside array with contain the maximum and minimum value of the column in the same index in the "columns" paramter.
+    IMPORTANT: the columns specified must have numerical values in at least 2 rows. It will return ['-inf', 'inf'] for columns that have no numerical values.
+    """
+    column_indeces = [-1] * len(columns)
+    result = []
+    for _ in range(len(columns)):
+      result.append([float('-inf'), float('inf')])
 
-  with open(file, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+    spamreader = csv.reader(self.file, delimiter = ',', quotechar = '|')
     header = next(spamreader)
 
     for index, column in enumerate(columns):
@@ -171,4 +169,4 @@ def max_min(file, columns):
         except Exception:
           pass
 
-  return result
+    return result
